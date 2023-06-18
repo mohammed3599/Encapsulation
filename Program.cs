6c53b05403e311e33a88f7af0d697e3edb8a74ef
mohammed3599 <kataloni1999@gmail.com>
2023-06-18 12:49:37 +0400

    second commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Encapsulation;
+
+namespace NameSpaceExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("Corolla", "2017", "Silver");
+            car.StartEngine();
+            car.moveCar();
+        }
+    }
+}
